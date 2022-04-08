4549fe7fe497eda58b0e1bd24b05ac7b06971e41
YangJiannan <yangjn1998@163.com>
2022-04-08 11:38:48 +0900

    excu

--- Solution104.java
+++
@@ -1,9 +0,0 @@
-public class Solution104 {
-
-    public int maxDepth(TreeNode root) {
-        if(root == null) return 0;
-        int leftHeight = maxDepth(root.left);
-        int rightHeight = maxDepth(root.right);
-        return Math.max(leftHeight,rightHeight)+1;
-    }
-}
--- Solution621.java
+++
@@ -1,7 +0,0 @@
-public class Solution621 {
-
-    public int leastInterval(char[] tasks, int n) {
-            if(tasks==null||tasks.length<=0) return 0;
-    }
-
-}
